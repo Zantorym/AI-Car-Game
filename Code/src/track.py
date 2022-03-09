d23253145d54adce04e70b99aa0f4c22658a0734
Jaspreet Singh Marwah <jaspreetsm123@gmail.com>
2022-03-08 19:18:35 -0500

    Exit handling & coloured tracks
    Exit handling (main.py): Changed the way the game exits in order to mitigate bug where code is trying to use pygame after calling pygame.quit().
    
    Coloured tracks (track.py): Tweaked code to be able to accept coloured images of tracks

--- Code/src/track.py
+++ Code/src/track.py
@@ -12,10 +12,10 @@ class Track:
 
     def setContours(self):
         image = cv2.imread(self.track_path)
-        im_bw = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        im_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         # min_thresh = 128 and max_thresh = 255
-        (thresh, im_bw) = cv2.threshold(im_bw, 128, 255, 0)
+        (thresh, im_bw) = cv2.threshold(im_bw, 50, 255, 0)
         contours, _ = cv2.findContours(im_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         return contours
 
