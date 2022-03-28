ccc0f89e7f4f634ac2511de0481deb8de4a52e5b
Terence Yu <terence.yu@dcmail.ca>
2022-03-28 14:41:56 -0400

    Fixed rendering of game control states

--- Code/main.py
+++ Code/main.py
@@ -1,12 +1,10 @@
+import sys
 import pygame
 import src.constants as CONSTANTS
 from src.game_state import GameState
 from src.car import Car, Steering, Acceleration
 from src.track import Track
 from src.commonUtils import print_text
-
-import sys
-
 from pygame.locals import (
     K_w,
     K_a,
@@ -33,50 +31,32 @@ key_strokes = {'w': False, 'a': False, 's': False, 'd': False}
 
 def show_key_strokes(surface, key_strokes):
     active = CONSTANTS.D_GREEN
-    default = CONSTANTS.BLACK
+    default = CONSTANTS.GREY
     w = active if key_strokes[K_w] else default
     a = active if key_strokes[K_a] else default
     s = active if key_strokes[K_s] else default
     d = active if key_strokes[K_d] else default
 
-    # pygame.draw.rect(SCREEN, w_bg, (905, 5, 40, 40), 2)  # W
-    # pygame.draw.rect(SCREEN, a_bg, (855, 55, 40, 40), 2)  # A
-    # pygame.draw.rect(SCREEN, s_bg, (905, 55, 40, 40), 2)  # S
-    # pygame.draw.rect(SCREEN, d_bg, (955, 55, 40, 40), 2)  # D
+    pygame.draw.rect(surface, w, (55, 7, 40, 40))
+    pygame.draw.rect(surface, a, (5, 53, 40, 40))
+    pygame.draw.rect(surface, s, (55, 53, 40, 40))
+    pygame.draw.rect(surface, d, (105, 53, 40, 40))
 
-    SCREEN.blit(CONSTANTS.W_FONT.render(f'W', True, w), dest=(907, 5))  # W
-    SCREEN.blit(CONSTANTS.A_FONT.render(f'A', True, a), dest=(857, 55))  # A
-    SCREEN.blit(CONSTANTS.S_FONT.render(f'S', True, s), dest=(907, 55))  # S
-    SCREEN.blit(CONSTANTS.D_FONT.render(f'D', True, d), dest=(957, 55))  # D
+    surface.blit(CONSTANTS.W_FONT.render(f'W', True, CONSTANTS.BLACK), dest=(65, 18))  # W
+    surface.blit(CONSTANTS.A_FONT.render(f'A', True, CONSTANTS.BLACK), dest=(17, 62))  # A
+    surface.blit(CONSTANTS.S_FONT.render(f'S', True, CONSTANTS.BLACK), dest=(67, 62))  # S
+    surface.blit(CONSTANTS.D_FONT.render(f'D', True, CONSTANTS.BLACK), dest=(117, 62))  # D
 
 
 def render_controls(surface, key_strokes):
-    # screen.fill(tuple(c.BLACK))
-    # screen.blit(back_image, back_rect)
-
-    # # TODO: Update car pos
-    # car.update()
-    # car.draw(screen)
-
-    # if action == 1:
-    #     pygame.draw.rect(screen, (0, 255, 0), (850, 50, 40, 40))
-    # if action == 2:
-    #     pygame.draw.rect(screen, (0, 255, 0), (800, 100, 40, 40))
-    # if action == 3:
-    #     pygame.draw.rect(screen, (0, 255, 0), (850, 100, 40, 40))
-    # if action == 4:
-    #     pygame.draw.rect(screen, (0, 255, 0), (900, 100, 40, 40))
-
-    # Key-strokes info
+    # Display visual indicator of keys pressed state
     show_key_strokes(surface, key_strokes)
 
-    # # score
-    # text_surface = c.POINTS_FONT.render(f'Points {car.points}', True, pygame.Color('green'))
-    # screen.blit(text_surface, dest=(0, 0))
-    # # speed
-    # text_surface = c.SPEED_FONT.render(f'Speed {car.vel * -1}', True, pygame.Color('green'))
-    # screen.blit(text_surface, dest=(420, 0))
-
+    # Display boxes as borders for keys
+    pygame.draw.rect(surface, CONSTANTS.BLACK, (55, 7, 40, 40), 2)  # W
+    pygame.draw.rect(surface, CONSTANTS.BLACK, (5, 53, 40, 40), 2)  # A
+    pygame.draw.rect(surface, CONSTANTS.BLACK, (55, 53, 40, 40), 2)  # S
+    pygame.draw.rect(surface, CONSTANTS.BLACK, (105, 53, 40, 40), 2)  # D
 
 
 def update_car(car, keys_pressed):
@@ -94,6 +74,7 @@ def update_car(car, keys_pressed):
         acceleration = Acceleration.BRAKE
 
     car.update(steering, acceleration)
+
 
 track_num = 2
 
@@ -157,7 +138,11 @@ def main():
         SCREEN.fill((255, 255, 255))
         SCREEN.blit(track.image, track.rect)
         car.draw(SCREEN)
-        render_controls(SCREEN, keys_pressed)
+        # Create surface for controls display
+        controls_surface = pygame.Surface((200, 100), pygame.SRCALPHA)
+        render_controls(controls_surface, keys_pressed)
+        SCREEN.blit(controls_surface, (900, 0))
+        # Draw rays on screen
         gamestate.draw_rays(SCREEN)
 
         # Collision Detection
@@ -182,59 +167,6 @@ def main():
 
     pygame.quit()
     sys.exit()
-
-    # while True:
-    #     clock.tick(c.FPS)
-    #     for event in pygame.event.get():
-
-    #         # Exiting the game
-    #         if event.type == pygame.QUIT:
-    #             pygame.quit()
-    #             sys.exit()
-
-    #         if event.type == c.COLLISION_EVENT:
-    #             # TODO: action to be done on collision
-    #             # car.respawn()
-    #             pygame.quit()
-    #             sys.exit()
-
-    #         # Checking user event
-    #         keys_pressed = pygame.key.get_pressed()
-    #         actions = []
-    #         action = 0
-    #         if keys_pressed[pygame.K_w]:
-    #             # Increase speed
-    #             action = 1
-    #         if keys_pressed[pygame.K_a]:
-    #             # Turn left
-    #             action = 2
-    #         if keys_pressed[pygame.K_s]:
-    #             # Decrease Speed
-    #             action = 3
-    #         if keys_pressed[pygame.K_d]:
-    #             # Turn right
-    #             action = 4
-    #         # TODO: do we want to implement a mechanism for constant speed without a key press
-    #         car.action(action)
-    #         actions.append(action)
-
-    #         # Store the user data for imitation learning
-    #         # wall_distance = car.get_wall_distance()
-    #         # data.append([wall_distance, actions])
-
-    #         # if event.type == env.terminate_run:
-    #         #     env.return_to_home()
-
-    #     key_strokes = {
-    #         'w': keys_pressed[pygame.K_w],
-    #         'a': keys_pressed[pygame.K_a],
-    #         's': keys_pressed[pygame.K_s],
-    #         'd': keys_pressed[pygame.K_d]
-    #     }
-
-    #     car.check_collision(track.getContours())
-    #     render(car, action, SCREEN, key_strokes)
-    #     pygame.display.update()
 
 
 if __name__ == '__main__':
